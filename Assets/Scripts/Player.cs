a51d4e2a66f8ff282b5d1b584d9796dae936e1ea
Untouchez <65210494+Untouchez@users.noreply.github.com>
2022-02-09 15:40:05 -0800

    climbing v1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class Player : MonoBehaviour
 {
 
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour
     public bool isSprinting;
     Animator anim;
     Camera mainCamera;
+    Rigidbody rb;
 
     Vector3 calculatedInput;
     Vector3 rawInput;
@@ -22,11 +23,21 @@ public class Player : MonoBehaviour
     public bool canMove;
     public bool canRotate;
     public bool lockedOn;
+
+    public bool closeToWall;
+    public bool isClimbing;
+
+    public float gravityForce;
+
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
     void Start()
     {
         //FINDS ANIMATOR FROM THE PLAYER GAMEOBJECT
         anim = GetComponent<Animator>();
-
+        rb = GetComponent<Rigidbody>();
         //GETS THE CAMERA FROM THE SCENE
         mainCamera = Camera.main;
 
@@ -44,8 +55,11 @@ public class Player : MonoBehaviour
         Debug.DrawRay(transform.position, transform.TransformDirection(rawInput),Color.blue);
 
         HandleInputs();
+        HandleClimb();
+        HandleMovement();
         HandleSprint();
         HandleAttack();
+        HandleGravity();
     }
 
     void FixedUpdate()
@@ -53,6 +67,65 @@ public class Player : MonoBehaviour
         HandleRotations();
     }
 
+    void HandleMovement()
+    {
+        if (!canMove)
+            return;
+        anim.SetFloat("Horizontal", calculatedInput.x);
+        anim.SetFloat("Vertical", calculatedInput.z);
+    }
+
+    void HandleClimb()
+    {
+        Ray ray = new Ray(transform.position + new Vector3(0, 1f, 0), transform.forward * 4f);
+        Debug.DrawRay(transform.position + new Vector3(0, 1f, 0), transform.forward * 4f, Color.green);
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (Physics.Raycast(ray, out RaycastHit hit, 0.7f)) //MIDDLE CHECK
+            {
+                transform.forward = -hit.normal;
+                isClimbing = !isClimbing;
+                anim.applyRootMotion = !isClimbing;
+                closeToWall = true;
+                anim.SetBool("climbing", isClimbing);
+                canRotate = !isClimbing;
+                canMove = !isClimbing;
+            } else {
+                anim.applyRootMotion = false;
+                isClimbing = false;
+                canMove = true;
+                canRotate = true;
+                anim.SetBool("climbing", false);
+            }
+        }
+
+        if (isClimbing) {
+            if (Physics.Raycast(ray, out RaycastHit hit, 0.7f)) {
+                transform.forward = -hit.normal;
+            } else {
+                anim.applyRootMotion = true;
+                isClimbing = false;
+                canMove = true;
+                canRotate = true;
+                anim.SetBool("climbing", false);
+            }
+        }
+        anim.SetFloat("Horizontal", calculatedInput.x);
+        anim.SetFloat("Vertical", calculatedInput.z);
+    }
+    void HandleGravity()
+    {
+        if(!isGrounded() && !isClimbing)
+        {
+            rb.AddForce(Vector3.down * gravityForce, ForceMode.Acceleration);
+        }
+    }
+    bool isGrounded()
+    {
+        if(Physics.Raycast(transform.position,-Vector3.down*4f,0.05f))       
+            return true;        
+        return false;
+    }
     void HandleSprint()
     {
         if(Input.GetKey(KeyCode.LeftShift)) {
@@ -87,14 +160,12 @@ public class Player : MonoBehaviour
         // D = 1 X
         rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         
-        if (rawInput.magnitude != 0 || !canMove)
+        if (rawInput.magnitude != 0)
             calculatedInput = new Vector3(Mathf.MoveTowards(calculatedInput.x, rawInput.x, acceleration * Time.deltaTime), 0, Mathf.MoveTowards(calculatedInput.z, rawInput.z, acceleration * Time.deltaTime));        
         else
             calculatedInput = new Vector3(Mathf.MoveTowards(calculatedInput.x, 0, acceleration * Time.deltaTime),0, Mathf.MoveTowards(calculatedInput.z, 0, decceleration * Time.deltaTime));
 
         calculatedInput = Vector3.ClampMagnitude(calculatedInput, 1);
-        anim.SetFloat("Horizontal", calculatedInput.x);
-        anim.SetFloat("Vertical", calculatedInput.z);
     }
 
     void HandleAttack()
